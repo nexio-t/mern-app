ff6ce7a3fb82f87077d0fdc8a496fdaa7b2d856d
Tomas Gear <tomas.e.gear@gmail.com>
2020-01-02 20:16:38 -0500

    updated YT API key

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,7 +4,7 @@ export default {
   
   // Search for videos 
   getYouTubeVideo: function(query) {
-    return axios.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q="+ query + "&key=AIzaSyAyOp0O_wd9CsT0kDl4REtZdaqgoQV0f6c"); 
+    return axios.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q="+ query + "&key=AIzaSyDkmcrvS7DR6DEF99WDcOYhXFC-2hO8KDA"); 
   },
   // Save a video
   saveVideo: function (newVideo) {
